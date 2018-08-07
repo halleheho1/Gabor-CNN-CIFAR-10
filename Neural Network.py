48ed099feb4042ebbd9f88f4895d7080f38803d9
halleheho <hallehehosg@hotmail.com>
2018-08-07 06:24:27 +0100

    add .py file

---
+++ Neural Network.py
@@ -0,0 +1,120 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import keras
+from skimage.filters import gabor_kernel
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers import Activation
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.utils import np_utils
+from keras_sequential_ascii import sequential_model_to_ascii_printout
+from keras import backend as K
+if K.backend()=='tensorflow':
+    K.set_image_dim_ordering("tf")
+from skimage.color import rgb2gray
+from scipy import ndimage as ndi
+ 
+# Import Tensorflow with multiprocessing
+import tensorflow as tf
+import multiprocessing as mp
+ 
+# Loading the CIFAR-10 datasets
+from keras.datasets import cifar10
+
+
+# In[18]:
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+y_train = np_utils.to_categorical(y_train, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
+x_train = np.load('convolved_x_train_cv2.npy')
+x_test = np.load('convolved_x_test_cv2.npy')
+
+
+# In[19]:
+
+
+batch_size = 32
+# 32 examples in a mini-batch, smaller batch size means more updates in one epoch
+
+num_classes = 10
+epochs = 24
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+# In[20]:
+
+
+def base_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same', data_format='channels_last', input_shape=x_train.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+    # sgd = SGD(lr = 0.1, decay = 1e-6, momentum=0.9, nesterov=True)
+    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+    # Train model
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+    return model
+
+
+# In[22]:
+
+
+print(x_train.shape)
+print(y_train.shape)
+cnn_n = base_model()
+cnn_n.summary()
+
+cnn = cnn_n.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=True)
+
+
+# In[24]:
+
+
+score = cnn_n.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+
+# In[29]:
+
+
+# serialize model to JSON
+model_json = cnn_n.to_json()
+with open("models/model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+cnn_n.save_weights("models/model.h5")
+print("Saved model to disk")
+
