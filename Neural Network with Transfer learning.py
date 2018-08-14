2e5fb3c468f7417e5deadc592b0a9c3077d7eebb
halleheho <hallehehosg@hotmail.com>
2018-08-15 00:44:53 +0100

    add tf model

--- Neural Network with Transfer learning.py
+++ Neural Network with Transfer learning.py
@@ -33,37 +33,10 @@ import multiprocessing as mp
 # Loading the CIFAR-10 datasets
 from keras.datasets import cifar10
 from keras.models import model_from_json
+import cv2
 
 
-# In[2]:
-
-
-json_file = open('models/model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("models/model.h5")
-print("Loaded model from disk")
-
-
-# ### Freeze first layer
-
-# In[3]:
-
-
-loaded_model.layers[0].trainable = False
-
-
-# In[4]:
-
-
-# loaded_model.summary(line_length=150)
-# loaded_model.pop()
-# len(loaded_model.layers)
-
-
-# In[5]:
+# In[9]:
 
 
 batch_size = 32
@@ -74,33 +47,176 @@ class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', '
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 
-# In[8]:
+# In[10]:
 
 
-y_train = np_utils.to_categorical(y_train, num_classes)
-y_test = np_utils.to_categorical(y_test, num_classes)
-x_train = np.load('convolved_x_train_cv2.npy')
-x_test = np.load('convolved_x_test_cv2.npy')
+def grayscale(data, dtype='float32'):
+    # luma coding weighted average in video systems
+    r, g, b = np.asarray(.3, dtype=dtype), np.asarray(.59, dtype=dtype), np.asarray(.11, dtype=dtype)
+    rst = r * data[:, :, :, 0] + g * data[:, :, :, 1] + b * data[:, :, :, 2]
+    return rst
+
+
+# In[11]:
+
+
+def add_dimension(data):
+    data = np.array([data])
+    #re arange the dimension
+    print(data.shape)
+    data = np.einsum('hijk->ijkh', data)
+    return data
+
+
+# In[12]:
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+train_selected_amount = 50000
+test_selected_amount = 10000
+num_classes = 10
+
+init_y_train = y_train[:train_selected_amount]
+init_y_test = y_test[:test_selected_amount]
+
+x_train = add_dimension(grayscale(x_train[:train_selected_amount]))
+x_test = add_dimension(grayscale(x_test[:test_selected_amount]))
+y_train = np_utils.to_categorical(init_y_train, num_classes)
+y_test = np_utils.to_categorical(init_y_test, num_classes)
+
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train  /= 255
 x_test /= 255
 
 
-# In[7]:
+# In[13]:
 
 
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-loaded_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-cnn_history = loaded_model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=True)
+def custom_gabor(shape, dtype=None):
+    pi = np.pi
+    orientation_spread = np.array([0, pi/4, pi/2, pi*3/4, pi, pi*5/4, pi*3/2, 2*pi])
+    scales = np.linspace(2, 3, 6)
+    real_kernels = []
+#     size, sigma, theta, lambda, gamma aspect ratio
+    for orientation in orientation_spread:
+        for scale in scales:
+            real_kernel = cv2.getGaborKernel((5, 5), 1, orientation, scale, 1, 0)
+            real_kernels.append(real_kernel)
+    real_kernels = np.array([real_kernels])
+    real_kernels = np.einsum('hijk->jkhi', real_kernels)
+    print(real_kernels.shape)
+
+    real_kernels = K.variable(real_kernels)
+    random = K.random_normal(shape, dtype=dtype)
+    return real_kernels
+
+
+# In[14]:
+
+
+def base_model(shape):
+    model = Sequential()
+    model.add(Conv2D(48, (3, 3), padding='same',kernel_initializer=custom_gabor, data_format='channels_last', input_shape=shape))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+    # sgd = SGD(lr = 0.1, decay = 1e-6, momentum=0.9, nesterov=True)
+    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+    # Train model
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+    return model
+
+
+# In[15]:
+
+
+# json_file = open('models/model5x5.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+model = base_model(x_train.shape[1:])
+model.load_weights("models/model5x5.h5")
+print("Loaded model from disk")
+
+
+# ### Freeze first layer
+
+# In[16]:
+
+
+model.layers[0].trainable = False
 
 
 # In[ ]:
 
 
-score = loaded_model.evaluate(x_test, y_test, verbose=0)
+# loaded_model.summary(line_length=150)
+# loaded_model.pop()
+# len(loaded_model.layers)
+
+
+# In[ ]:
+
+
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+cnn = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=True)
+
+
+# In[ ]:
+
+
+score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+
+# In[ ]:
+
+
+# Plots for training and testing process: loss and accuracy
+
+plt.figure(0)
+plt.plot(cnn.history['acc'],'r')
+plt.plot(cnn.history['val_acc'],'g')
+plt.xticks(np.arange(0, epochs, 2.0))
+plt.rcParams['figure.figsize'] = (8, 6)
+plt.xlabel("Num of Epochs")
+plt.ylabel("Accuracy")
+plt.title("Training Accuracy vs Validation Accuracy")
+plt.legend(['train','validation'])
+
+
+plt.figure(1)
+plt.plot(cnn.history['loss'],'r')
+plt.plot(cnn.history['val_loss'],'g')
+plt.xticks(np.arange(0, epochs, 2.0))
+plt.rcParams['figure.figsize'] = (8, 6)
+plt.xlabel("Num of Epochs")
+plt.ylabel("Loss")
+plt.title("Training Loss vs Validation Loss")
+plt.legend(['train','validation'])
+
+
+plt.show()
 
 
 # In[ ]:
